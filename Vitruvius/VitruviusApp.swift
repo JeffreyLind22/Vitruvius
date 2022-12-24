1b7c903737dadcf4698d84ccd17fbff5ed696ac4
Jeffrey Lind <jeffrey.lind24@pm.me>
2022-12-23 23:03:07 -0500

    Initial Skeleton Codebase

--- Vitruvius/VitruviusApp.swift
+++ Vitruvius/VitruviusApp.swift
@@ -1,17 +1,15 @@
-//
-//  VitruviusApp.swift
-//  Vitruvius
-//
-//  Created by Jeffrey Lind on 12/23/22.
-//
-
 import SwiftUI
 
 @main
+
 struct VitruviusApp: App {
+    
     var body: some Scene {
+        
         WindowGroup {
+            
             ContentView()
+            
         }
     }
 }
