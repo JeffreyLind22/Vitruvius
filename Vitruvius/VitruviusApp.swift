626f974c20a85d9f96a20d3c8fd6afd316295060
jeffreylind <Jeffrey.lind24@pm.me>
2022-12-23 22:31:33 -0500

    Initial Commit

---
+++ Vitruvius/VitruviusApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitruviusApp.swift
+//  Vitruvius
+//
+//  Created by Jeffrey Lind on 12/23/22.
+//
+
+import SwiftUI
+
+@main
+struct VitruviusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
