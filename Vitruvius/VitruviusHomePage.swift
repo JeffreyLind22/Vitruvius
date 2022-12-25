7bef652cabe14a517857b7ea78673e5e105c9341
Jeffrey Lind <jeffrey.lind24@pm.me>
2022-12-24 20:08:48 -0500

    Code + File Naming Relevancy Update

---
+++ Vitruvius/VitruviusHomePage.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+struct VitruviusHomePage: View {
+    
+    var body: some View {
+        
+        // Enter UI Here
+        Text( "Placeholder Text :)" )
+        
+    }
+}
+
+
+// XCode Preview Stuff
+struct XcodePreview: PreviewProvider {
+    
+    static var previews: some View {
+        
+        VitruviusHomePage()
+        
+    }
+}
